cb818faa2a4ba03ae16684034b22859a8584e019
Adam Hay <adam-hay@outlook.com>
2022-09-12 16:31:58 +0800

    added basic unoptimized read of crontab-file

--- estimatecron.c
+++ estimatecron.c
@@ -1,118 +1,127 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-
-
-// int64_t milliseconds(void)
-// {
-//     struct timeval  now;
-
-//     gettimeofday( &now, NULL );       // timezone not required, so we pass NULL
-//     return ((int64_t)now.tv_sec * 1000) + now.tv_usec/1000;
-// }
-
-// void readfile(char filename[], int buffersize)
-// {
-//     int     ncalls  = 0;
-//     int64_t start   = milliseconds();
-
-//     int fd  = open(filename, O_RDONLY);
-//     ++ncalls;
-
-//     if(fd >= 0) {
-//         char buffer[buffersize];
-//         size_t  got;
-
-//         while((got = read(fd, buffer, sizeof(buffer))) > 0) {
-//             ++ncalls;
-//         }
-//         close(fd);
-//         ++ncalls;
-//     }
-
-//     printf("%i\t%i\n", buffersize, (int)(milliseconds() - start));
-// //    printf("%i\t%i\n", ncalls, (int)(milliseconds() - start));
-// }
-
-
 int main(int argc, char *argv[])
 {
-   // check arguments
+   // check number of arguments
    if (argc != 4)
    {
       fprintf(stderr, "Usage: %s month crontab-file estimates-file\n", argv[0]);
       exit(EXIT_FAILURE);
-
    }
-
-   // convert month from argv to number
-   // check input length
+   // validate arg lengths
    if (strlen(argv[1]) > 3)
    {
       fprintf(stderr, "Error1: invalid second argument (month); must be either an integer 0..11, or a 3-character name");
       exit(EXIT_FAILURE);
-
    }
+
+   // init varibles
    int month = -1;
    char *months[] = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
 
-   // if not a number check month array
-   if (atoi(argv[1]) == 0)
+   // Validate and set month for month names
+   if (atoi(argv[1]) == 0 && strcmp(argv[1], "0") != 0)
    {
       for (int n = 0; n < 12; n++)
       {
          if (strcmp(months[n], argv[1]) == 0)
          {
-            month = n;
-            break;
+            month = n + 1;
          }
       }
-      // if not found, error
       if (month == -1)
       {
          fprintf(stderr, "Error: invalid second argument (month); 3-character month name not found");
          exit(EXIT_FAILURE);
-
       }
    }
-   // if number given
+   // Validate and set month for numbers
    else
    {
-      // convert num if in range, else error
-      if (atoi(argv[1]) < 12 && atoi(argv[1]) >= 0)
+      if (atoi(argv[1]) <= 12 && atoi(argv[1]) >= 1)
       {
          month = atoi(argv[1]);
-         exit(EXIT_FAILURE);
-}
+      }
       else
       {
          fprintf(stderr, "Error: invalid second argument (month); %s out of range must be between 0 and 11", argv[1]);
          exit(EXIT_FAILURE);
-}
+      }
    }
-   printf("Month argument: %d\n", month);
 
-  
-
-   // TODO: STORE PRINTED LINE in array !!!!!!!!!!!!!!!!!!!!!!!, then split into 2d/3d array
-   FILE* ptr;
-   int line_length=100;
+   // Setup variables for reading file
+   FILE *ptr;
+   int line_length = 100;
    char str[line_length];
+
+   //open file and handle instances that the file doesnt open properly
    ptr = fopen(argv[2], "r");
- 
-   if (NULL == ptr) {
+   if (NULL == ptr)
+   {
       fprintf(stderr, "Error: file can't be opened \n");
       exit(EXIT_FAILURE);
-}
-
-   while (fgets(str, line_length, ptr) != NULL) {
-      if (str[0]!='#')
-      printf("%s", str);
    }
 
+   // create struct for storing each line of crontab-file
+   struct command_cron
+   {
+      int min;   // min (0 - 59)
+      int hour;  // hour (0 - 23)
+      int daym;  // day of month (1 - 31)
+      int month; // month (1 - 12)
+      int dayw;  // day of week (0 - 6)
+      char command[40];
+   } crontab_data[20];
+
+   // count non-comment lines
+   int lines = 0;
+
+   while (fgets(str, line_length, ptr) != NULL)
+   {
+
+      if (str[0] != '#')
+      {
+         // crontab_data[lines].min = atoi(strtok(str, " "));
+         char *array[6];
+
+         int i = 0;
+         char *p = strtok(str, " ");
+
+         while (p != NULL)
+         {
+            array[i++] = p;
+            p = strtok(NULL, " ");
+         }
+
+         // printf("%s\n", array[i]);
+
+         crontab_data[lines].min = atoi(array[0]);
+         crontab_data[lines].hour = atoi(array[1]);
+         crontab_data[lines].daym = atoi(array[2]);
+         crontab_data[lines].month = atoi(array[3]);
+         crontab_data[lines].dayw = atoi(array[4]);
+         strcpy(crontab_data[lines].command, array[5]);
+         lines++;
+      }
+   }
    fclose(ptr);
-   
+
+
+//print out everything that we need
+   for (int i = 0; i < lines; i++)
+   {
+      printf("%d ", crontab_data[i].min);
+      printf("%d ", crontab_data[i].hour);
+      printf("%d ", crontab_data[i].daym);
+      printf("%d ", crontab_data[i].month);
+      printf("%d ", crontab_data[i].dayw);
+      printf("%s", crontab_data[i].command);
+   }
+
+   printf("\nMonth: %d\n", month);
+   printf("commands: %lu\n", (sizeof(crontab_data)/sizeof(crontab_data[0])));
+
    return 0;
 }
